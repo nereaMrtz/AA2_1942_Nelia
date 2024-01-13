f87e3d8982d4db8cc8ee78cc9362906da789da91
Pau Amigó <pau.amigo@enti.cat>
2024-01-14 00:33:24 +0100

    comentado cosas xro no va

--- Nelia/source/LevelLoader.cpp
+++ Nelia/source/LevelLoader.cpp
@@ -2,38 +2,40 @@
 
 std::vector<Wave*> LevelLoader::LoadWaves(std::string path, float levelTime )
 {
+		std::vector<Wave*> waves;
+
 	rapidxml::xml_document<> doc;
 	std::ifstream file(path);
-	std::stringstream buffer;
-	buffer << file.rdbuf();
-	file.close();
-	std::string content(buffer.str());
-	doc.parse<0>(&content[0]);
+	if (file.is_open()) {
+		std::stringstream buffer;
+		buffer << file.rdbuf();
+		file.close();
+		std::string content(buffer.str());
+		doc.parse<0>(&content[0]);
 
-	std::vector<Wave*> waves;
 
-	rapidxml::xml_node<>* pRoot = doc.first_node();
+		rapidxml::xml_node<>* pRoot = doc.first_node();
 
-	levelTime = (stof((std::string)pRoot->first_node()->value()));
-	std::cout << levelTime << std::endl;
+		levelTime = (stof((std::string)pRoot->first_node()->value()));
+		std::cout << levelTime << std::endl;
 
-	for (rapidxml::xml_node<>* pNode = pRoot->first_node("wave")->first_node(); pNode; pNode = pNode->next_sibling()) {
+		/*for (rapidxml::xml_node<>* pNode = pRoot->first_node()->first_node(); pNode; pNode = pNode->next_sibling()) {
 
-		float startTime = stof((std::string)pRoot->value());
-		pRoot = pRoot->next_sibling();
-		std::string pattern = (std::string)pRoot->first_attribute()->value();
-		std::string type = (std::string)pRoot->value();
-		pRoot = pRoot->next_sibling();
-		int amount = stoi((std::string)pRoot->value());
+			float startTime = stof((std::string)pRoot->value());
+			pRoot = pRoot->next_sibling();
+			std::string pattern = (std::string)pRoot->first_attribute()->value();
+			std::string type = (std::string)pRoot->value();
+			pRoot = pRoot->next_sibling();
+			int amount = stoi((std::string)pRoot->value());
 
-		std::cout << startTime << std::endl;
-		std::cout << pattern << std::endl;
-		std::cout << type << std::endl;
-		std::cout << type << std::endl;
+			std::cout << startTime << std::endl;
+			std::cout << pattern << std::endl;
+			std::cout << type << std::endl;
+			std::cout << type << std::endl;
+		}*/
+
+
 	}
-
-
-
 
 	return waves;
 }
